060b9bb75cd1c9fa6f913691ef87b919c75699d2
Jon Herron <jon.herron@yahoo.com>
2022-11-23 17:45:54 -0500

    WIP

--- examples/move_bpmn/main.py
+++ examples/move_bpmn/main.py
@@ -1,16 +1,15 @@
 from move_bpmn import MoveBPMNFiles
 
-def copy_bpmn_files():
-    print("copy bpmn files")
-
 if __name__ == "__main__":
-    scripting_additions = {
-        "copy_bpmn_files": copy_bpmn_files,
-    }
-
-    workflow = MoveBPMNFiles(
-        scripting_additions=scripting_additions,
-    )
-    
+    workflow = MoveBPMNFiles()
     result = workflow.run()
+
+    moved_files = result["move_targets"]
+
+    if len(moved_files) == 0:
+        print("No downloaded bpmn files found to move")
+    
+    for moved_file in moved_files:
+        print(f"Moved: {moved_file[0]} -> {moved_file[1]}")
+
     print(result)
